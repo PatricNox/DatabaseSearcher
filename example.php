8e55c8070f08ce59bfbd3c727eec684555181ef4
PatricNox <eronoxsmail2@gmail.com>
2018-03-25 21:16:09 +0200

    [PHP] Add DBS Usage Example

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+    require('dbSearcher/core.php'); // Load DBS-Core
+?>
+
+<html>
+    <head>
+        <title>Database Searcher Example</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <!-- Generate DBS markup view -->
+        <?php _DBSearcher();?>
+    </body>
+</html>
