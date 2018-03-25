3390f5d2688ea17e397fcc0a9b1f99df15736286
PatricNox <eronoxsmail2@gmail.com>
2018-03-25 21:15:31 +0200

    [PHP] Add Config file

---
+++ dbSearcher/configs/config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Database Connection Credentials */
+/***********************************/
+
+ $hostname  = 'localhost';    // The IP or DNS where the database server is located
+ $username  = 'root';         // Username for login to the database server
+ $password  = 'root';         // Password for login to the database server
