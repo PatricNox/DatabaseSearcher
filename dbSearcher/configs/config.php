b1d10ac93a6ef992b98d8448f6f30052f53b97ff
PatricNox <eronoxsmail2@gmail.com>
2018-09-05 22:10:06 +0200

    Implement config for Query-Execution time limit

--- dbSearcher/configs/config.php
+++ dbSearcher/configs/config.php
@@ -6,3 +6,9 @@
  $hostname  = 'localhost';    // The IP or DNS where the database server is located
  $username  = 'root';         // Username for login to the database server
  $password  = 'root';         // Password for login to the database server
+
+ /* Core Settings /*
+ /*****************/
+ 
+ // Execution time for queries
+ini_set('max_execution_time', 300); // 5 minutes
