3119e6b25e9096c8bd07c93cd82de84d5ef5516e
PatricNox <eronoxsmail2@gmail.com>
2018-12-14 18:08:48 +0100

    Update setup page design

--- dbSearcher/template.php
+++ dbSearcher/template.php
@@ -1,3 +1,23 @@
+	<form action='#' method='POST' class="clearfix">
+		<div>
+			<input class="float-l" style="width: 50%;" type="text" placeholder="searching for.." name="dbs-search">
+			<input class="float-r" style="width: 50%;" type="text" placeholder="in database" name="dbs-database">
+		</div>
+		<br />
+		<br />
+		<br />
+		<div class="clearfix"><input class="float-r" type="submit" value="Search"></div>
+	</form>
+	<?php if (!$QueryFound ):?>
+		<p>More accurate search words = better results!<br><br><br>
+		<a href="setup.php">
+			<div class="clearfix"><input class="float-l" type="submit" value="Setup Database"></div>
+		</a>
+	<?php endif; ?>
+	<?php if(isset($search)): ?>
+		<h2>Searched for: "<?=(isset($search)) ?$search:'';?>" in (<?=(isset($database)) ?$database:'';?>)</h2>
+	<?php endif; ?>
+	
 	<?php if ($QueryFound && $search): ?>
 		<table align="center">
 			<h2>Query Results:</h2>
